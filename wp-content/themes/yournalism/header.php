3f74370b4ede9f05ead8c26a08b2dda2b344b952
Huub Schuijn <huubschuijn@MacBook-Pro-van-Huub.local>
2014-07-06 00:37:06 +0200

    single page translation

--- wp-content/themes/yournalism/header.php
+++ wp-content/themes/yournalism/header.php
@@ -9,14 +9,22 @@
 
 <nav>
 	<div class="topbar container flex-row">
-		<div class="flex"><a href="<?php bloginfo('home'); ?>"><img class="logo no-flex" src="<?php bloginfo('template_url'); ?>/images/yournalism.png" alt=""></a></div>
-		<div class="flex">
+		<div class="no-flex logo"><a href="<?php bloginfo('home'); ?>"><img class="no-flex" src="<?php bloginfo('template_url'); ?>/images/yournalismlogowit.png" alt=""></a></div>
+		<div class="flex"></div>
+		<div class="no-flex">
 			<ul>
 				<li><a href="/about">yournalism is</a></li>
-				<li><a href="/team">who are we</a></li>
-				<li><a href="/projects">proposals</a></li>
+				<li><a href="/team">wie zijn wij</a></li>
+				<li><a href="/projects">bekijk voorstellen</a></li>
 			</ul>
 		</div>
-		<div class="no-flex login">my yournalism</div>
+		<div class="flex"></div>
+		<div class="no-flex login">
+			<span class="icon"><i class="fa fa-fw fa-twitter-square"></i></span>
+			<span class="icon"><i class="fa fa-fw fa-facebook-square"></i></span>
+			<span class="icon"><i class="fa fa-fw fa-linkedin-square"></i></span>
+			<span class="icon"><i class="fa fa-fw fa-google-plus-square"></i></span>
+		</div>
+		<div class="subtitle">Jij bepaalt het volgende verhaal.</div>
 	</div>
 </nav>
--- wp-content/themes/yournalism/page.php
+++ wp-content/themes/yournalism/page.php
@@ -9,7 +9,7 @@
 
 	<div class="container project">
 		<div class="header">
-			<div class="project-type">Proposal</div>
+			<div class="project-type">Voorstel</div>
 			<div class="project-title"><?php the_title(); ?></div>
 			<div class="author">
 				<div class="author-image" style="background-image: url(<?php bloginfo('template_url'); ?>/images/author-huub.jpg);"></div>
@@ -20,67 +20,39 @@
 		<div class="project-info">
 			<div class="info-type">
 				<div class="number">80</div>
-				<div class="description">people backed</div>
+				<div class="description">mensen doneerden</div>
 			</div>
 			<div class="info-type">
 				<div class="number">&euro;2500</div>
-				<div class="description">as a target</div>
+				<div class="description">is het doel</div>
 			</div>
 			<div class="info-type">
-				<div class="number">12 hours</div>
-				<div class="description">timelimit</div>
+				<div class="number">12 uren</div>
+				<div class="description">te gaan</div>
 			</div>
 			<div class="progressbar">
 				<div class="completed" style="width: calc(70% - 10px);">70%</div>
 			</div>
-			<div class="button">donate <i class="fa fa-play"></i></div>
+			<div class="button">doneer <i class="fa fa-play"></i></div>
 		</div>
 	
 		<div class="content">
 			<div><?php the_content(); ?></div>
 	
 			<div class="social">
-				<p>Let your friends get involved</p>
+				<p>Deel dit met je vrienden</p>
 				<p>
 					<span class="icon"><i class="fa fa-fw fa-twitter"></i></span>
 					<span class="icon"><i class="fa fa-fw fa-facebook"></i></span>
 					<span class="icon"><i class="fa fa-fw fa-linkedin"></i></span>
-					<span class="icon"><i class="fa fa-fw fa-rss"></i></span>
+					<span class="icon"><i class="fa fa-fw fa-google-plus"></i></span>
 				</p>
 			</div>
 
 			<div class="section-related">
-				<h2>Related proposals</h2>
+				<h2>Andere voorstellen</h2>
 				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
-					<a href="">titel moet hier nog komen</a>
-				</div>
-				<div class="related">
-					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/blaat.png)"></div>
+					<div class="related-image" style="background-image:url(<?php bloginfo('template_url');?>/images/author-huub.jpg)"></div>
 					<a href="">titel moet hier nog komen</a>
 				</div>
 				<div class="clearfix"></div>
@@ -89,6 +61,8 @@
 		</div>
 
 	</div>
+
+
 
 <?php endwhile; ?>
 
